1071a8f78cf4bd4172ffa224fd25db176ffd7c3c
Alex Cândido <alex.candido.tec@gmail.com>
2023-01-22 19:00:29 -0300

    estrutura inicial de getters em store/todo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,9 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  nitro: {
+    externals: {
+      inline: ["uuid"],
+    }
+  }
 })
--- store/todo.ts
+++ store/todo.ts
@@ -1,12 +1,50 @@
 import { defineStore } from "pinia";
+import { v4 as uuid } from "uuid";
 
-const state = () => ({
+export interface Todo {
+  id: string;
+  title: string;
+  done: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface TodoAdd {
+  title: string;
+}
+
+export interface TodoState {
+  items: Todo[];
+}
+
+const state = (): TodoState => ({
   items: [],
 })
 
-const getters = {}
+const getters = {
+  getById: (state: TodoState) => (id: string) => {
+    return state.items.find((item:Todo) => item.id === id);
+  },
+  getOrderedTodos: (state: TodoState) => 
+  state.items.sort(
+    (a: Todo, b: Todo) => 
+    a.createdAt.getMilliseconds() - b.createdAt.getMilliseconds()
+    ),
+}
 
-const actions ={}
+const actions ={
+  add(partialTodo: TodoAdd) {
+    const todo: Todo = {
+      id: uuid(),
+      ...partialTodo,
+      done: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    this.items.push(todo);
+  },
+
+}
 
 export const useTodoStore = defineStore('todoStore', {
   state,
