0fe0d00f6a867dc8e2c94aad9cf5232f6dd75598
Alex Cândido <alex.candido.tec@gmail.com>
2023-02-05 16:48:01 -0300

    adicionado funções deleteTodo e updateTodoDone em TodoItem.vue

--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-import { CheckCircleIcon, XCircleIcon } from '@heroicons/vue/24/solid';
-import { Todo } from '~~/store/todo';
-
+  import { CheckCircleIcon, XCircleIcon } from '@heroicons/vue/24/solid';
+import { Todo, useTodoStore } from '~~/store/todo';
 
   const props = defineProps <{
     todo: Todo;
   }>();
+
+  const todoStore = useTodoStore();
+  const deleteTodo = (id: string) => todoStore.remove(id);
+
+  const updateTodoDone = (todo: Todo) => {
+    const currentState = todo.done;
+    todoStore.update(todo.id, { done: !currentState })
+  }
   
 </script>
 
