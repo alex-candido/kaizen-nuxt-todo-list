666a8a3c865ab32a3320d8746200c82d9a0e8087
Alex Cândido <alex.candido.tec@gmail.com>
2023-02-25 23:02:53 -0300

    adicionado middleware de redirecionamento em auth.global.ts

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,14 +1,28 @@
 <script lang="ts" setup>
+  const email = ref("");
+  const password = ref("");
+  const passwordConfirm = ref("");
+  const loading = ref(false);
+  const error = ref(null);
 
+  const loginMode = ref(true);
+
+  const authorize = async () => {
+    let err = "Login in..."
+    console.log(err)
+  }
 </script>
 
 <template>
   <main class="min-h-screen pt-18 bg-gray-100">
     <section class="text-center py-10">
-      <h1 class="text-5xl font-bold text-gray-700 mb-4">Login to your account</h1>
+      <h1 class="text-5xl font-bold text-gray-700 mb-4">
+        {{ loginMode ? "Login to your account" : "Create an account" }}  
+      </h1>
 
       <form class="flex flex-col max-w-lg mx-auto" @submit.prevent>
         <input 
+          v-model="email"
           class="px-2 py-3 mt-3 border-1 border-lime-200"
           placeholder="Email Address"
           type="email"
@@ -16,6 +30,7 @@
           id="email"
         />
         <input
+          v-model="password"
           class="px-2 py-3 mt-3 border-1 border-lime-200"
           placeholder="Password"
           type="password"
@@ -24,6 +39,8 @@
         /> 
 
         <input 
+          v-if="!loginMode"
+          v-model="passwordConfirm"
           class="px-2 py-3 mt-3 border-1 border-lime-200"
           placeholder="Re-enter your password"
           type="password"
@@ -31,13 +48,24 @@
           id="password-confirm"
         />
 
-        <p v-if="false" class="text-red-400 mt-3">
-          Error
+        <button
+          class="p-5 bg-blue-500 text-white rounded-sm mt-5 disabled:bg-gray-400 disabled:text-white"
+          :disabled="loading"
+          @click="authorize"
+        >
+          Authorize
+        </button>
+
+        <p v-if="error" class="text-red-400 mt-3">
+          {{ error }}
         </p>
         <a 
+          @click="$event => loginMode = !loginMode"
           class="mt-3 underline cursor-pointer"
         >
-          Login
+          Or
+          {{ loginMode ? "Register" : "Login"}}
+          instead
         </a>
       </form>
     </section>
